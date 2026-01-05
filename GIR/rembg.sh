d3a257b66ee1943f7f5849e89afedd0100fddd1b
HeliaCheng <heliacheng127@gmail.com>
2026-01-05 14:44:01 +0800

    rembg & relighting

---
+++ GIR/rembg.sh
@@ -0,0 +1,2 @@
+# 批量处理 input_dir 所有图片，仅输出 Mask 到 output_mask_dir
+rembg p ../Relightable3DGaussian/FA_scene/gs_results/chair/fibo/img ../Relightable3DGaussian/FA_scene/gs_results/chair/fibo/masks --only-mask
